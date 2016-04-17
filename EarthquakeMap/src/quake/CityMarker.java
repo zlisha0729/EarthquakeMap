a5da9ee1d78b7ed0bb3b8e75d3b3509b08beb9b8
Lisha Zhou <zhoulisha0729@gmail.com>
2016-04-17 13:06:50 -0400

    refactor cityMarker to implement drawMarker and showTitle function in abstract class CommonMarker

--- EarthquakeMap/src/quake/CityMarker.java
+++ EarthquakeMap/src/quake/CityMarker.java
@@ -3,14 +3,13 @@ package quake;
 import de.fhpotsdam.unfolding.data.Feature;
 import de.fhpotsdam.unfolding.data.PointFeature;
 import de.fhpotsdam.unfolding.geo.Location;
-import de.fhpotsdam.unfolding.marker.SimplePointMarker;
+import processing.core.PConstants;
 import processing.core.PGraphics;
 
 
-public class CityMarker extends SimplePointMarker {
+public class CityMarker extends CommonMarker {
 	
-	// The size of the triangle marker
-	public static final int TRI_SIZE = 5;  
+	public static int TRI_SIZE = 5;  // The size of the triangle marker
 	
 	public CityMarker(Location location) {
 		super(location);
@@ -19,35 +18,58 @@ public class CityMarker extends SimplePointMarker {
 	
 	public CityMarker(Feature city) {
 		super(((PointFeature)city).getLocation(), city.getProperties());
+		// Cities have properties: "name" (city name), "country" (country name)
+		// and "population" (population, in millions)
 	}
 	
 	
-	
-	public void draw(PGraphics pg, float x, float y) {
+	/**
+	 * Implementation of method to draw marker on the map.
+	 */
+	public void drawMarker(PGraphics pg, float x, float y) {
+		//System.out.println("Drawing a city");
 		// Save previous drawing style
 		pg.pushStyle();
 		
-		// draw the CityMarker
 		pg.fill(150, 30, 30);
 		pg.triangle(x, y-TRI_SIZE, x-TRI_SIZE, y+TRI_SIZE, x+TRI_SIZE, y+TRI_SIZE);
-
+		
 		// Restore previous drawing style
 		pg.popStyle();
 	}
 	
-	public String getCity()
+	/** Show the title of the city if this marker is selected */
+	public void showTitle(PGraphics pg, float x, float y)
+	{
+		String name = getCity() + " " + getCountry() + " ";
+		String pop = "Pop: " + getPopulation() + " Million";
+		
+		pg.pushStyle();
+		
+		pg.fill(255, 255, 255);
+		pg.textSize(12);
+		pg.rectMode(PConstants.CORNER);
+		pg.rect(x, y-TRI_SIZE-39, Math.max(pg.textWidth(name), pg.textWidth(pop)) + 6, 39);
+		pg.fill(0, 0, 0);
+		pg.textAlign(PConstants.LEFT, PConstants.TOP);
+		pg.text(name, x+3, y-TRI_SIZE-33);
+		pg.text(pop, x+3, y - TRI_SIZE -18);
+		
+		pg.popStyle();
+	}
+	
+	private String getCity()
 	{
 		return getStringProperty("name");
 	}
 	
-	public String getCountry()
+	private String getCountry()
 	{
 		return getStringProperty("country");
 	}
 	
-	public float getPopulation()
+	private float getPopulation()
 	{
 		return Float.parseFloat(getStringProperty("population"));
 	}
-	
 }
